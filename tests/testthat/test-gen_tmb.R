cdca6689252f8e1461f0bd2c9f708b742ec96044
Sean Anderson <sean@seananderson.ca>
2020-10-23 11:27:17 -0700

    Fix test

--- tests/testthat/test-gen_tmb.R
+++ tests/testthat/test-gen_tmb.R
@@ -1,7 +1,7 @@
 context("gen_tmb inputs")
 
 test_that("gen_tmb works", {
-  x <- gen_tmb(catch_dat, comp_dat)
+  x <- gen_tmb(catch_ex, comp_ex)
   expect_true("pars" %in% names(x))
   expect_equal(class(x), "list")
 })
