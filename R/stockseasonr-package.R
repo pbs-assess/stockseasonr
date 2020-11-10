bc55d1cacdb326b0dd7d5d29c3a3c4655bb3bffe
Sean Anderson <sean@seananderson.ca>
2020-11-09 16:05:03 -0800

    R CMD check fixes

--- R/stockseasonr-package.R
+++ R/stockseasonr-package.R
@@ -8,18 +8,28 @@
 ## usethis namespace: end
 NULL
 
-utils::globalVariables(c(
-  ".",
-  "agg",
-  "agg_prob",
-  "area",
-  "month",
-  "month_n",
-  "nn",
-  "reg_month",
-  "region",
-  "sample_id",
-  "strata",
-  "total_obs"
-))
-
+if (getRversion() >= "2.15.1") {
+  utils::globalVariables(c(
+    ".",
+    "agg",
+    "agg_prob",
+    "area",
+    "month",
+    "month_n",
+    "nn",
+    "reg_month",
+    "region",
+    "sample_id",
+    "strata",
+    "total_obs",
+    "link_abund_est",
+    "link_abund_se",
+    "link_abund_low",
+    "link_abund_up",
+    "pred_prob_est",
+    "link_prob_est",
+    "link_prob_se",
+    "link_abund_up",
+    "stock"
+  ))
+}
